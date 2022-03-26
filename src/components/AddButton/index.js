3a6681acd3c634e08e79c5b79596ae5f9de630a3
Monika Nawój <nawojmonika@gmail.com>
2022-03-26 22:00:03 +0100

    added add movie button

---
+++ src/components/AddButton/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddButton = () => {
+    return (
+        <button className="btn btn-success">
+            <i className="bi bi-plus"></i>Dodaj do koszyka
+        </button>
+    );
+}
+
+export default AddButton;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,52 +1,58 @@
-import React, {useContext} from "react";
-import {DataContext} from "../../contexts/DataContext";
+import React, { useContext } from "react";
+import { DataContext } from "../../contexts/DataContext";
 import SortingButton from "../../components/SortingButton";
+import AddButton from "../../components/AddButton";
 
 const Home = () => {
-    const {data} = useContext(DataContext);
+    const { data } = useContext(DataContext);
 
     return (
         <>
+            <h2>Dostępne filmy do wypożyczenia</h2>
             <table className="table table-striped align-middle">
                 <thead className="table-dark align-middle">
-                <tr>
-                    <th scope="col">
-                        <SortingButton title={'Tytuł'} fieldName={'title'}/>
-                    </th>
-                    <th scope="col">Gatunek</th>
-                    <th scope="col">Zdjęcie</th>
-                    <th scope="col">Opis</th>
-                    <th scope="col">Język</th>
-                    <th scope="col">Rok produkcji</th>
-                    <th scope="col">Cena</th>
-\                </tr>
+                    <tr>
+                        <th scope="col">
+                            <SortingButton title={'Tytuł'} fieldName={'title'} />
+                        </th>
+                        <th scope="col">Gatunek</th>
+                        <th scope="col">Zdjęcie</th>
+                        <th scope="col">Opis</th>
+                        <th scope="col">Język</th>
+                        <th scope="col">Rok produkcji</th>
+                        <th scope="col">Cena</th>
+                        <th scope="col"></th>
+                    </tr>
                 </thead>
                 <tbody>
-                {data?.map(({id, title, genre, url, desc, lang, year, price}) =>
-                    <tr key={id}>
-                        <td className="col-2"><strong>{title}</strong></td>
-                        <td>
-                            {genre?.map(name => (
-                                <span className="badge bg-secondary">{name}</span>
-                            ))}
-                        </td>
-                        <td>
-                            <img src={url} className="img-thumbnail mx-auto d-block" alt={title}/>
-                        </td>
-                        <td>{desc}</td>
-                        <td>
-                            {lang?.map(name => (
-                                <span className="badge bg-info">{name}</span>
-                            ))}
-                        </td>
-                        <td>
-                            <span className="badge bg-dark">{year}</span>
-                        </td>
-                        <td>
-                            {price} PLN
-                        </td>
-                    </tr>
-                )}
+                    {data?.map(({ id, title, genre, url, desc, lang, year, price }) =>
+                        <tr key={id}>
+                            <td className="col-2"><strong>{title}</strong></td>
+                            <td>
+                                {genre?.map(name => (
+                                    <span className="badge bg-secondary">{name}</span>
+                                ))}
+                            </td>
+                            <td>
+                                <img src={url} className="img-thumbnail mx-auto d-block" alt={title} />
+                            </td>
+                            <td>{desc}</td>
+                            <td>
+                                {lang?.map(name => (
+                                    <span className="badge bg-info">{name}</span>
+                                ))}
+                            </td>
+                            <td>
+                                <span className="badge bg-dark">{year}</span>
+                            </td>
+                            <td>
+                                {price} PLN
+                            </td>
+                            <td>
+                                <AddButton />
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </>
