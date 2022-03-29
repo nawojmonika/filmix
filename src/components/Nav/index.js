a60d0f67c0fcdf184a7b40ea18411226ff43ac47
Monika Nawój <nawojmonika@gmail.com>
2022-03-29 19:43:35 +0200

    fixed branding link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,7 +7,7 @@ const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Filmix                     </a>
+                <CustomLink className="navbar-brand" to='/'>Filmix</CustomLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
