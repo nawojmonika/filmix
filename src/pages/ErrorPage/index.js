d8518e40d00bd7485e73e3d06be07076496983bb
Monika Nawój <nawojmonika@gmail.com>
2022-03-29 22:19:53 +0200

    404 page styles

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './errorPage.css';
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="error">
             <h2>Błąd 404</h2>
+            <i class="bi bi-exclamation-octagon"></i>
             <p>To nie jest strona, której szukasz</p>
         </div>
     );
