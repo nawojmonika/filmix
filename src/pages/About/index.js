68f71861dc2293e21ff50a56e218104aa4cb4a0f
Monika Nawój <nawojmonika@gmail.com>
2022-03-30 00:23:26 +0200

    filled about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,20 +1,33 @@
 import React from "react";
+import "./about.css";
 
 const About = () => {
     return (
-        <div>
-            <h2>Kilka słów o wirtualnej wypożyczalni filmów</h2>
-            <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus porta erat in efficitur. Nam consectetur tortor sit amet odio cursus mattis eu ut dolor. Morbi vestibulum, nibh sit amet malesuada commodo, nisi est pharetra justo, ac lacinia nisi enim eu ex. Maecenas a est sit amet mi hendrerit consequat sed eu mi. Fusce eget leo id tellus consectetur vestibulum. Duis sed est porta, pretium sapien eu, auctor erat. In quis lacinia leo. Donec gravida dictum dolor, vitae commodo nisi ultricies ut. Nulla suscipit augue at placerat semper. Nam velit risus, lacinia vel felis ut, gravida cursus justo. Aenean euismod ex at ante efficitur mollis. Vestibulum tempus leo posuere, lobortis nibh a, faucibus massa. Suspendisse tincidunt maximus lectus nec facilisis. Praesent accumsan lorem ac mi pulvinar, non scelerisque odio porta. Sed eleifend ultrices quam, nec aliquam erat luctus sit amet.
-
-                Vivamus fermentum ligula ante, a laoreet enim interdum at. Suspendisse orci lectus, pharetra eget volutpat in, finibus id quam. Mauris eget elit nunc. Duis nec lacus id sem rutrum pulvinar mattis et nibh. Vestibulum ac sagittis enim. Donec leo turpis, facilisis at risus vel, faucibus sagittis nulla. Etiam neque sapien, dapibus vitae venenatis eu, viverra sed tellus. Donec varius eget mauris sit amet sollicitudin. Aliquam ut orci ut justo laoreet vulputate non vel libero. Aenean a erat rutrum, ultricies enim a, volutpat nibh. Vestibulum et convallis felis. Sed semper dui quis laoreet lobortis. Duis sem eros, imperdiet non lectus sed, elementum consectetur eros.
-
-                Pellentesque pulvinar elit eu diam molestie, vel molestie justo pharetra. Nulla facilisis eros vel interdum sodales. Praesent eu urna id arcu aliquet facilisis. Phasellus lacus lectus, gravida in nisi quis, hendrerit blandit risus. Proin finibus blandit nunc ac ornare. Vivamus ultrices, dui vitae ultricies luctus, enim urna varius massa, a eleifend felis dui in eros. Proin rhoncus eleifend magna id consectetur.
-
-                In urna est, luctus at eros at, suscipit feugiat magna. Praesent interdum venenatis scelerisque. Cras eget lectus a sapien lobortis pulvinar. Nullam vestibulum orci blandit ullamcorper volutpat. Maecenas aliquam bibendum sagittis. In finibus arcu in ligula lacinia pellentesque. Ut at nulla id urna tempor scelerisque. Aliquam scelerisque eu neque vel ultricies.
-
-                Quisque tortor est, hendrerit id lacinia vitae, ornare ac tortor. Proin et fringilla mi. Sed efficitur nulla mauris, id lobortis tortor accumsan sit amet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus suscipit sem at est vulputate mattis. Duis quis lorem sit amet tortor facilisis tincidunt sit amet at diam. Mauris rutrum egestas nisi, sit amet rutrum felis porta fermentum. Duis eros lorem, vehicula vel sollicitudin eget, fermentum at libero. Duis commodo est purus. Aenean commodo tempor tincidunt. Ut porta leo quis eros interdum eleifend.
-            </div>
+        <div className="about">
+            <h2>Kilka słów o nas:</h2>
+            <ol>
+                <li>Strona poświęcona jest wirtualnej wypożyczalni filmów "Filmex"</li>
+                <li>
+                    Użyte technologie:
+                    <ul>
+                        <li>React.js</li>
+                        <li>Bootstrap 5</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                    </ul>
+                </li>
+                <li>Spełnione wymagania projektu:
+                    <ul>
+                        <li>Serwis został zrealizowany na bazie HTML5 </li>
+                        <li>Formatowanie przy użyciu CSS</li>
+                        <li>Serwis funkcjonuje poprawnie i wygląda dobrze we wszystkich popularnych przeglądarkach</li>
+                        <li>Serwis jest dostępny dla niepełnosprawnych: został użyty semantyczny html, wszystkie obrazki posiadają tekst alternatywny, użyte zostały atrybuty aria</li>
+                        <li>Został użyty <span>localstorage</span> do przechowywania zawartości koszyka</li>
+                        <li>System posiada nawigacje oraz 5 podstron: strona domowa, o nas, regulamin, koszyk oraz strona błędu 404</li>
+                        <li>Serwis posiada podstronę poświęconą opisowi serwisu</li>
+                    </ul>
+                </li>
+            </ol>
         </div>
     )
 }
