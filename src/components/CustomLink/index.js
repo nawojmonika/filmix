51d38bf611cf13d608559989bfc00f9a41eff29d
Monika Nawój <nawojmonika@gmail.com>
2022-03-30 23:03:59 +0200

    navbar fix

--- src/components/CustomLink/index.js
+++ src/components/CustomLink/index.js
@@ -7,8 +7,8 @@ const CustomLink = ({children, to, ...props}) => {
     let match = useMatch({ path: resolved.pathname, end: true });
 
     return (
-        <li className="nav-item">
-            <Link className={`nav-link ${match ? 'active' : ''}`} to={to} {...props} aria-current={match && 'page'}>{children}</Link>
+        <li className={`nav-item ${match ? 'active' : ''}`}>
+            <Link className="nav-link" to={to} {...props} aria-current={match && 'page'}>{children}</Link>
         </li>
     );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,23 +5,21 @@ import "./Nav.css"
 
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <CustomLink className="navbar-brand" to='/'>Filmix</CustomLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <CustomLink to='/'>Strona główna</CustomLink>
-                        <CustomLink to='/o-nas'>O nas</CustomLink>
-                        <CustomLink to='/regulamin'>Regulamin</CustomLink>
-                        <CustomLink to='/koszyk'><i className="bi bi-basket"/> Koszyk</CustomLink>
-                    </ul>
-                </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <CustomLink className="navbar-brand" to='/'>Filmix</CustomLink>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <CustomLink to='/'>Strona główna</CustomLink>
+                    <CustomLink to='/o-nas'>O nas</CustomLink>
+                    <CustomLink to='/regulamin'>Regulamin</CustomLink>
+                    <CustomLink to='/koszyk'><i className="bi bi-basket" /> Koszyk</CustomLink>
+                </ul>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Nav;
